292040b6215130c44692ce8eff6e2097a66ce051
Gautam Mehta <gautam.mehta@techcefz.com>
2020-03-29 04:44:44 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const db=require('./db');
+const collection='todo';
+
+
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+}); 
+app.get('/getTodos',(req,res)=>{
+    db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+        if(err)
+        console.log(err);
+        else{
+            console.log(documents);
+            res.json(documents);
+        }
+    });
+});
+
+app.put('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    const userInput = req.body;
+    console.log(todoID);
+   console.log("req.body"+req.body);
+   
+
+
+    db.getDB().collection(collection).findOneAndUpdate({_id :db.getPrimaryKey(todoID)},{$set :{todo :userInput.todo}},{returnOriginal :false},(err,result)=>{
+        console.log("userInput.todo"+userInput.todo);
+         if(err)
+         console.log(err);
+         else
+         res.json(result);
+       //  console.log(userInput.todo);
+    });
+});
+
+app.post('/',(req,res)=>{
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+        if(err)
+        console.log(err);
+        else
+        res.json({result :result, document : result.ops[0]});
+    });
+});
+
+app.delete('/:id',(req,res)=>{
+    const todoID = req.params.id;
+    db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(todoID)},(err,result)=>{
+        if(err)
+        console.log(err);
+        else
+        res.json(result);
+    })
+})
+ 
+db.connect((err)=>{
+    if(err){
+    console.log('unable to connect to database');
+    process.exit(1);
+    }
+    else{
+        app.listen(3000,()=>{
+            console.log('connected to database ,app listening to port 3000');
+        });
+    }
+});
